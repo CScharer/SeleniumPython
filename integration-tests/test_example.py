23438aa4791e9384ca6c24c609f98335dfe60938
Christopher J. Scharer <ChrisScharer1416@msn.com>
2021-12-18 14:13:47 -0600

    Fixed Class name as it must start with Test.

--- integration-tests/test_example.py
+++ integration-tests/test_example.py
@@ -2,16 +2,15 @@ import pytest
 
 
 # python -m pytest -v -s
-# class Example:
-@pytest.mark.parametrize("field", [1, 2, 3])
-def test_example(field):
-    example: str = "Example"
-    print(f"{example}-{field}")
-    if field == 1:
-        assert False, f"Testing Assertion"
+class TestExample:
+    @pytest.mark.parametrize("field", [1, 2, 3])
+    def test_example(self, field):
+        example: str = "Example"
+        print(f"{example}-{field}")
+        if field == 1:
+            assert False, f"Testing Assertion"
 
-
-def test_example2():
-    example: str = "Example2"
-    print(f"{example}")
-    assert True, f"Testing Assertion"
+    def test_example2(self):
+        example: str = "Example2"
+        print(f"{example}")
+        assert True, f"Testing Assertion"
